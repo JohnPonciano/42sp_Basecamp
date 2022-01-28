23dd5c15705c86eb019b566fc520a908184dc9d5
JohnPonciano <jhow.ponciano@hotmail.com>
2022-01-28 13:57:01 +0100

    first commit

---
+++ olamundo.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+
+	int A, B;
+
+	printf("\n Digite o Valor 1: \t");
+	scanf("%d",&A);
+	printf("\n Digite o Valor 2: \t");
+	scanf("%d",&B);;
+
+return 0 ;
+}
